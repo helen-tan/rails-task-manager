91ad647275dd5a589ba2c2372335f4c72dea8d7a
helen tan <helentanmz0909@gmail.com>
2022-05-25 14:26:40 +0800

    Edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   # See all tasks
   get 'tasks', to: 'tasks#index'
 
-  # Create a task: get restaurant creation form + post to db
+  # Create a task: get task creation form + post to db
   get "tasks/new", to: "tasks#new", as: :new_task # new needs to be before show
   post "tasks", to: "tasks#create"
 
   # See the details of one task
   get "tasks/:id", to: "tasks#show", as: :task
+
+  # Update a task: get task editting form + patch to db
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
 end
